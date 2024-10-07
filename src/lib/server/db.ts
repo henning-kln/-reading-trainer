f81b6c363b5d9906e4ccacfcf8808f470d4e7df7
Henning Klein <info@henningklein.de>
2024-10-07 19:47:27 +0200

    add better caching

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,5 +1,5 @@
 import {PrismaClient} from "@prisma/client"
-import { getCache, setCache } from "./cache";
+import { deleteCache, getCache, setCache } from "./cache";
 
 const prisma = new PrismaClient()
 
@@ -11,6 +11,7 @@ export async function createWord(word: string): Promise<void> {
                 text: word
             }
         });
+        deleteCache('words');
     } catch (error) {
         console.error("Error creating word:", error);
     }
@@ -38,6 +39,7 @@ export async function deleteWord(word: string): Promise<void> {
         await prisma.word.delete({
             where: {text: word}
         });
+        deleteCache('words');
     } catch (error) {
         console.error("Error deleting word:", error);
     }
@@ -62,16 +64,21 @@ export async function setGhostRate(ghostRate: number): Promise<void> {
             where: {key: 'ghost-rate'},
             data: {value: ghostRate.toString()}
         })
+        setCache('ghost-rate', ghostRate);
     } catch (error) {
         console.error("Error setting ghost rate:", error);
     }
 }
 export async function getGhostRate(): Promise<number> {
+    if(getCache('ghost-rate')){
+        return getCache('ghost-rate') as number;
+    }
     try {
         const ghostRate = await prisma.settings.findUnique({
             where: {key: 'ghost-rate'}
         });
         if(!ghostRate) return 0;
+        setCache('ghost-rate', Number(ghostRate.value));
         return Number(ghostRate.value);
     } catch (error) {
         console.error("Error getting ghost rate:", error);
@@ -97,16 +104,21 @@ export async function setTimer(timer: number): Promise<void> {
             where: {key: 'timer'},
             data: {value: timer.toString()}
         })
+        setCache('timer', timer);
     } catch (error) {
         console.error("Error setting timer:", error);
     }
 }
 export async function getTimer(): Promise<number> {
+    if(getCache('timer')){
+        return getCache('timer') as number;
+    }
     try {
         const timer = await prisma.settings.findUnique({
             where: {key: 'timer'}
         });
         if(!timer) return 0;
+        setCache('timer', Number(timer.value));
         return Number(timer.value);
     } catch (error) {
         console.error("Error getting timer:", error);
