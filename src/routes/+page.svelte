f8907b04fa6f63b0635986b0f250f8e05523c8d5
Henning Klein <info@henningklein.de>
2024-10-07 20:34:13 +0200

    add syllabify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,31 @@
 
 	export let data 
 
-	let word = "";
+	let words :(string|null)[]= [];
 	let started = false;
 	let interval: string | number | NodeJS.Timeout | undefined;
-
+	let ghost = false;
 	const startTraining = ()=>{
 		started = true;
 		interval = setInterval(async () => {
 			const res = await fetch("/api/random-word")
-			word = await res.text();		
+			const text = await res.text();
+			ghost = text === "GHOST";
+			if(!ghost){
+				words = [text].flatMap(word => syllabify(word) || []);
+			}
+				
 		}, data.props.time*1000);
 	}
 
 	const stopTraining = ()=>{
 		clearInterval(interval);
 		started = false;
+	}
+	const syllableRegex = /[^aeiouy]*[aeiouy]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?/gi;
+
+	function syllabify(words: string) {
+		return words.match(syllableRegex);
 	}
 
 	
@@ -32,11 +42,15 @@
 	<img src="" alt="">
 	<hr class="w-11/12">
 	<div class="flex flex-col items-center justify-center min-h-96 h-full">
-		{#if word === "GHOST"}
+		{#if ghost}
 			<img src="/ghost.png" alt="ghost" class="w-1/2">
 			<h2 class="text-4xl">Huuuuiiiii</h2>
 		{:else}
-			<h2 class="text-4xl ">{word}</h2>
+			<h2 class="text-4xl silben">
+				{#each words as word}
+					<span>{word}</span>
+				{/each}
+			</h2>
 		{/if}
 	</div>
 {:else}
@@ -47,3 +61,14 @@
 	
 {/if}
 
+<style>
+	.silben :nth-child(odd){
+		color: red;
+	}
+	.silben :nth-child(even){
+		color: blue;
+	}
+	.silben {
+		@apply bg-white
+	}
+</style>
